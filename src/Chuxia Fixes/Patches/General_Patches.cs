3d973df157e6c23cc6c02ffda418d8a7b2950f8b
chuxiaaaa <1612121215@qq.com>
2025-11-08 05:58:42 +0800

    1.0.2

--- src/Chuxia Fixes/Patches/General_Patches.cs
+++ src/Chuxia Fixes/Patches/General_Patches.cs
@@ -17,36 +17,37 @@ using TMPro;
 using Unity.Netcode;
 
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 namespace Patches
 {
     [HarmonyWrapSafe]
     public static class General_Patches
     {
-        [ThreadStatic]
-        private static TMP_Text? currentProcessingText;
+        //[ThreadStatic]
+        //private static TMP_Text? currentProcessingText;
 
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(TextMeshProUGUI), nameof(TextMeshProUGUI.SetArraySizes))]
-        [HarmonyPatch(typeof(TextMeshPro), nameof(TextMeshPro.SetArraySizes))]
-        public static void SetArraySizesPrefix(TextMeshProUGUI __instance)
-        {
-            if (Plugin.General_DisableFontWarn.Value)
-            {
-                currentProcessingText = __instance;
-            }
-        }
+        //[HarmonyPrefix]
+        //[HarmonyPatch(typeof(TextMeshProUGUI), nameof(TextMeshProUGUI.SetArraySizes))]
+        //[HarmonyPatch(typeof(TextMeshPro), nameof(TextMeshPro.SetArraySizes))]
+        //public static void SetArraySizesPrefix(TextMeshProUGUI __instance)
+        //{
+        //    if (Plugin.General_DisableFontWarn.Value)
+        //    {
+        //        currentProcessingText = __instance;
+        //    }
+        //}
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(TextMeshProUGUI), nameof(TextMeshProUGUI.SetArraySizes))]
-        [HarmonyPatch(typeof(TextMeshPro), nameof(TextMeshPro.SetArraySizes))]
-        public static void SetArraySizesPostfix(TextMeshProUGUI __instance)
-        {
-            if (Plugin.General_DisableFontWarn.Value)
-            {
-                currentProcessingText = null;
-            }
-        }
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(TextMeshProUGUI), nameof(TextMeshProUGUI.SetArraySizes))]
+        //[HarmonyPatch(typeof(TextMeshPro), nameof(TextMeshPro.SetArraySizes))]
+        //public static void SetArraySizesPostfix(TextMeshProUGUI __instance)
+        //{
+        //    if (Plugin.General_DisableFontWarn.Value)
+        //    {
+        //        currentProcessingText = null;
+        //    }
+        //}
 
 
         [HarmonyPostfix]
@@ -55,29 +56,27 @@ namespace Patches
         {
             if (Plugin.General_DisableFontWarn.Value)
             {
-                if (currentProcessingText != null)
-                {
-                    var stopwatch = Stopwatch.StartNew();
-
-                    var input = currentProcessingText.text;
-                    StringBuilder stringBuilder = new StringBuilder(input.Length);
-                    foreach (char c in input)
-                    {
-                        if (char.IsControl(c))
-                        {
-                            stringBuilder.Append(c);
-                        }
-                        else if (currentProcessingText.font.HasCharacter(c, false, false))
-                        {
-                            stringBuilder.Append(c);
-                        }
-                        else
-                        {
-                            stringBuilder.Append('□');
-                        }
-                    }
-                    currentProcessingText.text = stringBuilder.ToString();
-                }
+                //if (currentProcessingText != null)
+                //{
+                //    var input = currentProcessingText.text;
+                //    StringBuilder stringBuilder = new StringBuilder(input.Length);
+                //    foreach (char c in input)
+                //    {
+                //        if (char.IsControl(c))
+                //        {
+                //            stringBuilder.Append(c);
+                //        }
+                //        else if (currentProcessingText.font.HasCharacter(c, false, false))
+                //        {
+                //            stringBuilder.Append(c);
+                //        }
+                //        else
+                //        {
+                //            stringBuilder.Append('□');
+                //        }
+                //    }
+                //    currentProcessingText.text = stringBuilder.ToString();
+                //}
                 __result = true;
             }
         }
@@ -96,18 +95,43 @@ namespace Patches
             }
         }
 
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SceneEventData), "AddSpawnedNetworkObjects")]
+        public static void AddSpawnedNetworkObjects(SceneEventData __instance)
+        {
+            if (Plugin.General_DestroyOpenedGift.Value)
+            {
+                if (__instance?.m_NetworkObjectsSync == null) return;
+                for (int i = __instance.m_NetworkObjectsSync.Count - 1; i >= 0; i--)
+                {
+                    var item = __instance.m_NetworkObjectsSync[i];
+                    if (item == null) continue;
+
+                    var grab = item.GetComponentInChildren<GrabbableObject>();
+                    if (grab is GiftBoxItem gift && gift.hasUsedGift)
+                    {
+                        Plugin.Log.LogInfo($"[GiftBox] Remove {item.GetInstanceID()}");
+                        __instance.m_NetworkObjectsSync.RemoveAt(i);
+                        UnityEngine.Object.Destroy(item.gameObject);
+                    }
+                }
+            }
+        }
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(StartOfRound), "SetShipReadyToLand")]
         private static void ResetHUDManager()
         {
-            if (!Plugin.General_FixDeathBoxes.Value)
+            if (Plugin.General_FixDeathBoxes.Value)
             {
-                return;
+                var hudManager = HUDManager.Instance;
+                if (hudManager == null) return;
+                hudManager.spectatingPlayerBoxes = new Dictionary<Animator, PlayerControllerB>();
+                hudManager.boxesAdded = 0;
             }
-            var hudManager = HUDManager.Instance;
-            if (hudManager == null) return;
-            hudManager.spectatingPlayerBoxes = new Dictionary<Animator, PlayerControllerB>();
-            hudManager.boxesAdded = 0;
         }
+
+
     }
 }
